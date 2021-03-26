32b408a02c17215f3567b4540f188c5176f0d38e
Adam Natrop <adamnatrop@gmail.com>
2021-03-26 10:25:35 -0500

    fixed merge

--- assets/js/script_propagate.js
+++ assets/js/script_propagate.js
@@ -31,15 +31,12 @@ function propagateRecentSearch () {
 function propagateResultList () {
     $("#resultList").empty()
     $("#resultList")
-<<<<<<< HEAD
-    .append($("<p>").text(`${rep} from ${location}, could buy ${(Math.trunc(repWorth/itemPrice*100)/100).toLocaleString("en")} using thier net worth.`)
-    )
-    .append($("<p>").text(`The average constituent in ${location} could buy ${(Math.trunc(avConstWorth/itemPrice*100)/100).toLocaleString("en")} `)
-=======
+
+
     .append($("<p>").text(`${repName} from ${chosenStateName}, could buy ${(Math.trunc(repWorth/itemPrice*100)/100)} using thier net worth.`)
     )
     .append($("<p>").text(`The average constituent in ${chosenStateName} could buy ${Math.trunc(chosenStateNetWorth/itemPrice*100)/100}`)
->>>>>>> 7755de3ba42d4175eb46ec205169b0127c1b400a
+
     )
 }
 
